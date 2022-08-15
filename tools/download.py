51e48463a64793e54c14576658f8e362661010db
Rajat Rasal <rajat@rebelliondefense.com>
2022-08-15 23:17:11 +0100

    Refactored downloads.py with requirements

--- tools/download.py
+++ tools/download.py
@@ -1,25 +1,36 @@
-from pytube import YouTube
 import argparse
 import os
 
-parser = argparse.ArgumentParser(description='Download YouTube videos')
-parser.add_argument('--videolist', default='./configs/videolist.txt')
-parser.add_argument('--videopath', default='videos')
+from pytube import YouTube
+from tqdm import tqdm
 
-args = parser.parse_args()
+
 if __name__ == '__main__':
+  parser = argparse.ArgumentParser(description='Download YouTube videos')
+  parser.add_argument('--videolist', default='./configs/videolist.txt')
+  parser.add_argument('--videopath', default='videos')
+  
+  args = parser.parse_args()
+
   if not os.path.exists(args.videopath):
     os.mkdir(args.videopath)
-  count=0
-  with open(args.videolist,"r") as f:
-    urls = f.readlines()
-  urls = [url.replace('\n','') for url in urls]
 
-  for url in urls:
+  count = 0
+
+  with open(args.videolist, "r") as f:
+    urls = [url.replace('\n','') for url in f.readlines()]
+
+  for url in tqdm(urls):
     try:
       video = YouTube(r"https://www.youtube.com/watch?v=" + url)
-      video.streams.filter(progressive=True, file_extension='mp4').order_by('resolution')[-1].download(args.videopath,filename=url)
+      video.streams \
+        .filter(progressive=True, file_extension='mp4') \
+        .order_by('resolution')[-1] \
+        .download(args.videopath, filename=url)
       count += 1
+      print(f"Processed {url}")
     except:
+      print(f"Cannot process {url}")
       continue
-  print(f"download: {count} videos || total: {len(urls)} videos")
+
+  print(f"Downloaded {count}/{len(urls)} videos")
